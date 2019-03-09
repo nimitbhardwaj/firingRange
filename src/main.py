d1095cf1e51254d10910e3165a45b3fc94168098
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2019-03-09 10:15:49 +0530

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from micAdapter import mic
+from server import server
+
+class Main(object):
+    @staticmethod
+    def main():
+        pass        
+
+
+if __name__ == '__main__':
+    Main.main()
---
+++ src/micAdapter/mic.py
@@ -0,0 +1,5 @@
+class Mic(object):
+    def __init__(self):
+        pass
+    def __register(self):
+        pass
